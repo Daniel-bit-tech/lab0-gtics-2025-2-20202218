b977e5806bacce347935dc372e8b7839bdf315a9
Daniel <Daniel@gmail.com>
2025-08-21 20:30:19 -0500

    feat:Imprmiendo Imprimiendo elementos

--- src/main/java/com/pucp/LAB020252/Lab020252Application.java
+++ src/main/java/com/pucp/LAB020252/Lab020252Application.java
@@ -12,7 +12,10 @@ public class Lab020252Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Lab020252Application.class, args);
 		System.out.println("Hola Mundo");
-
+		List<String> list = List.of("Elemento 1", "Elemento 2", "Elemento 3") ;
+		for (String s : list) {
+			System.out.println("Elemento " +s);
+		}
 	}
 
 }
